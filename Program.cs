7b3eb98f448df86475890b98d783f2de3b3f5fb4
Long18i1 <48077478+Long18i1@users.noreply.github.com>
2019-02-28 10:44:48 +0700

    Add files via upload
    em nộp bài ngày 28/02/2019

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doantau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tau sr = new tau();
+            sr.doan();
+            sr.Inmadoantau();
+            Console.ReadLine();
+        }
+    }
+}
