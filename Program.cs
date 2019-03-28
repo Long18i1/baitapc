8a28bc738a4ce8029ca274361aace1c7539748a7
Long18i1 <48077478+Long18i1@users.noreply.github.com>
2019-03-28 12:00:38 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SachGiaoKhoa sc = new SachGiaoKhoa();
+            sc.nhap();
+            sc.In();
+            Console.ReadLine();
+        }
+    }
+}
+;
---
+++ SachGiaoKhoa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sach
+{
+    class SachGiaoKhoa : Thongtinsach
+    {
+        String tinhtrangsach;
+        public SachGiaoKhoa() : base()
+        {
+            this.tinhtrangsach = "moi";
+        }
+        public new void nhap()
+        {
+            base.nhap();
+            Console.WriteLine("Tinh trang cua sach:");
+            this.tinhtrangsach = Console.ReadLine();
+        }
+        public double tinhtien()
+        {
+            double tt;
+            if (this.tinhtrangsach == "moi")
+            {
+                tt = this.soluong * this.dongia;
+            }
+            else
+            {
+                tt = this.soluong * this.dongia * 0.5;
+            }
+            return tt;
+        }
+        public void In()
+        {
+            base.In();
+            Console.WriteLine("Tong so tien la: {0}",this.tinhtien());
+        }
+    } 
+}
