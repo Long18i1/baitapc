5b060385137f7cfc12061e6a854f2bfadbe40436
Long18i1 <48077478+Long18i1@users.noreply.github.com>
2019-05-25 13:30:27 +0700

    Add files via upload
    Em nộp bài ngày 25/05/2019

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,8 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
 
-namespace testAPP
+namespace BaiTapNop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,34 +26,108 @@ namespace testAPP
         public MainWindow()
         {
             InitializeComponent();
+            FilldataGi();
         }
-
-        private void BtGT_Click(object sender, RoutedEventArgs e)
+        SqlConnection conn = null;
+        SqlCommand cmd = null;
+        SqlDataAdapter da;
+        DataSet ds;
+        public void ConnectDB()
         {
-            int n = Int32.Parse(txtSoN.Text);
-            double s = 1;
-            for ( int i = 2; i <=n; i++)
+            try
             {
-                s += Math.Sqrt(i);
+                conn = new SqlConnection("Data Source=DESKTOP-4H2CDN2\\SQLEXPRESS; Initial Catalog=BTC; Integrated Security=SSPI");
+                conn.Open();
             }
-            txtKQ.Text = s.ToString();
-        }
-
-        private void BtX_Click(object sender, RoutedEventArgs e)
-        {
-            int n = Int32.Parse(txtSOX.Text);
-            int x = 2;
-            double c = 0;
-            for (int i = 1; i <= n; i++)
+            catch (Exception ex)
             {
-                c += Math.Pow(x, i);
+                Console.WriteLine(ex.Message);
             }
-            txtKq.Text = c.ToString();
+        }
+        public void FilldataGi()
+        {
+            try
+            {
+                ConnectDB();
+                da = new SqlDataAdapter("select * from NopBT ", conn);
+                ds = new DataSet();
+                da.Fill(ds);
+                dataGi.ItemsSource = ds.Tables[0].DefaultView;
+                dataGi.DisplayMemberPath = "CustomerID";
+                dataGi.SelectedValuePath = "ComparyName";
+                conn.Close();
+                da.Dispose();
+                ds.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
-        private void BtM_Click(object sender, RoutedEventArgs e)
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
 
+                ConnectDB();
+                cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "Insert into NopBT values(@CustomerID,@ComparyName,@ContactName,@ContactTitle,@Addiess,@City,@Region,@PostaCode,@Country,@Phone,@Fax)";
+                cmd.Parameters.Add("@CustomerID", SqlDbType.VarChar).Value = txtCustomerID.Text;
+                cmd.Parameters.Add("@ComparyName", SqlDbType.VarChar).Value = txtComparyName.Text;
+                cmd.Parameters.Add("@ContactName", SqlDbType.VarChar).Value = txtContactName.Text;
+                cmd.Parameters.Add("@ContactTitle", SqlDbType.VarChar).Value = txtContactTitle.Text;
+                cmd.Parameters.Add("@Addiess", SqlDbType.VarChar).Value = txtAddiess.Text;
+                cmd.Parameters.Add("@City", SqlDbType.VarChar).Value = txtCity.Text;
+                cmd.Parameters.Add("@Region", SqlDbType.VarChar).Value = txtRegion.Text;
+                cmd.Parameters.Add("@PostaCode", SqlDbType.VarChar).Value = txtPostaCode.Text;
+                cmd.Parameters.Add("@Country", SqlDbType.VarChar).Value = txtCountry.Text;
+                cmd.Parameters.Add("@Phone", SqlDbType.VarChar).Value = txtPhone.Text;
+                cmd.Parameters.Add("@Fax", SqlDbType.VarChar).Value = txtFax.Text;
+                int n = cmd.ExecuteNonQuery();
+                if (n > 0) MessageBox.Show("Success");
+                else MessageBox.Show("Fail");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectDB();
+            cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "Delete from NopBT where CustomerID = '" + txtCustomerID.Text + "'";
+            int n = cmd.ExecuteNonQuery();
+            if (n > 0) MessageBox.Show("Success");
+            else MessageBox.Show("Fail");
+
+        }
+
+        private void BtnL_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectDB();
+            cmd = new SqlCommand();
+            cmd.Connection = conn;
+            var load = new MainWindow();
+            load.Show();
+            this.Hide();
+        }
+
+        private void BtnU_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectDB();
+            cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "Update NopBT Set ComparyName = '"+txtComparyName.Text+ "' Where CustomerID = '" + txtCustomerID.Text+ "' ";
+            int n = cmd.ExecuteNonQuery();
+            if (n > 0) MessageBox.Show("Success");
+            else MessageBox.Show("Fail");
+            conn.Close();
+            cmd.Dispose();
         }
     }
 }
---
+++ NguyenThaiLong18i1_25_5.sql
@@ -0,0 +1,26 @@
+USE [BTC]
+GO
+
+/****** Object:  Table [dbo].[NopBT]    Script Date: 25/5/2019 1:25:49 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[NopBT](
+	[CustomerID] [varchar](50) NULL,
+	[ComparyName] [varchar](50) NULL,
+	[ContactName] [varchar](50) NULL,
+	[ContactTitle] [varchar](50) NULL,
+	[Addiess] [varchar](50) NULL,
+	[City] [varchar](50) NULL,
+	[Region] [varchar](50) NULL,
+	[PostaCode] [varchar](50) NULL,
+	[Country] [varchar](50) NULL,
+	[Phone] [varchar](50) NULL,
+	[Fax] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+
+
